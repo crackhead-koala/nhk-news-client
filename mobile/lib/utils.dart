844f88e964c124fa085df26c96b21ca2d2f4cb32
Sergey Suvorov <suvorov@findmykids.org>
2024-02-25 00:47:57 +0100

    remove files

--- mobile/lib/utils.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:convert';
-import 'dart:io';
-
-List<dynamic> readJsonFile(String filePath) {
-  var input = File(filePath).readAsStringSync();
-  var map = jsonDecode(input);
-  return map;
-}
